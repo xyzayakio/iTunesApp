08ef7adbd8193561234ea200e49f3f59ea4e188e
Kelvin Cheong <xyzayakio@gmail.com>
2023-03-23 21:15:11 +0800

    Fix deprecated code

--- app/src/main/java/com/example/itunesapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/itunesapp/ui/theme/Theme.kt
@@ -3,17 +3,13 @@ package com.example.itunesapp.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
         primary = Purple80,
@@ -53,10 +49,13 @@ fun ITunesAppTheme(
         else -> LightColorScheme
     }
     val view = LocalView.current
+    val currentWindow = (view.context as? Activity)?.window
+        ?: throw Exception("Not in an activity - unable to get Window reference")
+
     if (!view.isInEditMode) {
         SideEffect {
             (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(currentWindow, view).isAppearanceLightStatusBars = darkTheme
         }
     }
 
