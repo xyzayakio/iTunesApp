fccf3bf9849f740acd77c7b0546b026e6a0ef960
Kelvin Cheong <xyzayakio@gmail.com>
2023-03-26 19:18:57 +0800

    Add api response data class

---
+++ app/src/main/java/com/example/itunesapp/model/GetAlbumListResponse.kt
@@ -0,0 +1,10 @@
+package com.example.itunesapp.model
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class GetAlbumListResponse(
+    @Json(name = "resultCount") val resultCount: Long?,
+    @Json(name = "results") val results: List<Album>?
+)
