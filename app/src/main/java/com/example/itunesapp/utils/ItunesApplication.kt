1b92053926a1b12fac671fb70f92b3f27b47816c
Kelvin Cheong <xyzayakio@gmail.com>
2023-03-26 19:20:30 +0800

    Dagger stuffs

---
+++ app/src/main/java/com/example/itunesapp/utils/ItunesApplication.kt
@@ -0,0 +1,16 @@
+package com.example.itunesapp.utils
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ItunesApplication: Application() {
+
+    lateinit var component: ApplicationComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        component = DaggerApplicationComponent.create()
+        component.inject(this)
+    }
+}
